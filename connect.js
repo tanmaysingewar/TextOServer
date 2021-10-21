0c3804f492b8e7a0ead986815f992d7283a8b98c
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2021-10-21 18:17:57 +0530

    last a commit

--- connect.js
+++ connect.js
@@ -1,12 +1,13 @@
 const mongodb = require('mongodb');
-const uri = 'mongodb://umaorj8arzmg4oltks9l:bGiA1SdJdCIPB9CRLiIN@bxilqqpqgp5xgvx-mongodb.services.clever-cloud.com:27017/bxilqqpqgp5xgvx';
+const uri = 'mongodb+srv://Tanmay:FicktreeTanmay@146@ficktree.bvlsf.mongodb.net/Ficktree?retryWrites=true&w=majority';
 const localuri = "mongodb://localhost:27017"
 
 export async function connectDb(){
     // connection to database
     const client = await mongodb.MongoClient.connect(uri,{ useUnifiedTopology: true });  //# connecting to DB
     //return connection
-    return client.db("New")  //# returning the connection on "New" DB 
+    return client.db("TextO")  //# returning the connection on "New" DB 
 }
 
 //token :   eyJhbGciOiJIUzI1NiJ9.NjE0NzIxMDU5ZGY5YzYwOTc1Y2Q3ZjEx.A7Sq4AsJfRYoNuOeH9oQ4drLOYZgrpDwsmnkD4styDo
+//const uri = 'mongodb+srv://Tanmay:FicktreeTanmay@146@ficktree.bvlsf.mongodb.net/Ficktree?retryWrites=true&w=majority';
